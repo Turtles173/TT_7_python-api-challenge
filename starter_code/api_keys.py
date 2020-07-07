dcfdf8613c8419dc256eb398d1f209667f36d902
Tas <tas0173@gmail.com>
2020-07-07 14:58:48 +1000

    Images for homework submission

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ae93c3dda480aac27a90a69e059b9292"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAUCod1NY2fyoXgooC7hEUIJxZ8nnXoSME"
